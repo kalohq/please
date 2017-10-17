ebd3c3a6e3c23f0a530d28d92f337751d12d32a7
Tomek Wiszniewski <tomek@lystable.com>
2017-10-17 21:48:24 +0200

    Prettier + ESLint all the things

--- source/please.js
+++ source/please.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const path = require('path');
 const fs = require('fs');
 const tinyError = require('tiny-error');
@@ -7,21 +5,19 @@ const childProcess = require('child_process');
 const permissions = require('mode-to-permissions');
 const hasbin = require('hasbin');
 
-const fileInfo = (filePath) => {
+const fileInfo = filePath => {
   let stats;
   try {
     stats = fs.statSync(filePath);
   } catch (error) {
     if (error.code !== 'ENOENT') throw error;
-    return { exists: false, executable: false };
+    return {exists: false, executable: false};
   }
 
-  if (
-    !stats.isFile() ||
-    !permissions(stats).execute.owner
-  ) return { exists: true, executable: false };
+  if (!stats.isFile() || !permissions(stats).execute.owner)
+    return {exists: true, executable: false};
 
-  return { exists: true, executable: true };
+  return {exists: true, executable: true};
 };
 
 function findScriptsDirs(directories, currentPath) {
@@ -36,10 +32,9 @@ function findScriptsDirs(directories, currentPath) {
     return findScriptsDirs(directories, nextPath);
   }
 
-  const nextDirectories = (scriptsStat.isDirectory()
+  const nextDirectories = scriptsStat.isDirectory()
     ? directories.concat([scriptsPath])
-    : directories
-  );
+    : directories;
   if (nextPath === currentPath) return nextDirectories;
   return findScriptsDirs(nextDirectories, nextPath);
 }
@@ -48,20 +43,18 @@ function findScriptsDirs(directories, currentPath) {
  * Takes raw CLI `args` and a global `process`. Returns the exit code.
  */
 module.exports = (args, globals) => {
-  const process = globals.process;
+  const {process} = globals;
 
   if (args[0] === '--help') {
     if (hasbin.sync('man')) {
       childProcess.spawnSync(
         'man',
         [path.resolve(__dirname, '../manpages/please.1')],
-        { stdio: [process.stdout, process.stdin] }
+        {stdio: [process.stdout, process.stdin]}
       );
     } else {
-      const fallbackPath =
-        path.resolve(__dirname, '../manpages/please.1.txt');
-      const plainTextHelp =
-        fs.readFileSync(fallbackPath, 'utf8');
+      const fallbackPath = path.resolve(__dirname, '../manpages/please.1.txt');
+      const plainTextHelp = fs.readFileSync(fallbackPath, 'utf8');
       process.stdout.write(plainTextHelp);
     }
     return 0;
@@ -69,66 +62,62 @@ module.exports = (args, globals) => {
 
   const cwd = process.cwd();
   const scriptsDirs = findScriptsDirs([], cwd);
-  if (!scriptsDirs.length) throw tinyError(
-    'Hmmm. We’ve looked everywhere, but we can’t find a scripts directory. ' +
-    'Make sure your current working directory or any of its ancestors ' +
-    'contains a subdirectory named `scripts`.'
-  );
+  if (!scriptsDirs.length)
+    throw tinyError(
+      'Hmmm. We’ve looked everywhere, but we can’t find a scripts directory. ' +
+        'Make sure your current working directory or any of its ancestors ' +
+        'contains a subdirectory named `scripts`.'
+    );
 
   if (!args.length) {
-    const allScripts = scriptsDirs.map((dir) => (
-      fs.readdirSync(dir).filter(file => (
-        fileInfo(path.join(dir, file)).executable
-      ))
-    ));
+    const allScripts = scriptsDirs.map(dir =>
+      fs
+        .readdirSync(dir)
+        .filter(file => fileInfo(path.join(dir, file)).executable)
+    );
     const allScriptsFlat = Array.prototype.concat.apply([], allScripts);
 
     /* eslint-disable prefer-template */
     // So how do you propose to format this, ESLint?
     process.stdout.write(
       '\n' +
-      'Available commands:\n' +
-      allScriptsFlat.map(script => `  ${script}\n`).join('') +
-      '\n'
+        'Available commands:\n' +
+        allScriptsFlat.map(script => `  ${script}\n`).join('') +
+        '\n'
     );
     /* eslint-enable prefer-template */
     return 0;
   }
 
-  const script = args[0];
-  const scriptArgs = args.slice(1);
-
-  const relativeScriptPath = (dir) => path.join(
-    path.relative(cwd, dir),
-    script
-  );
-
-  const scriptsPathData = scriptsDirs.map((dir) => ({
+  const [script, ...scriptArgs] = args;
+  const relativeScriptPath = dir => path.join(path.relative(cwd, dir), script);
+  const scriptsPathData = scriptsDirs.map(dir => ({
     dir,
     file: path.resolve(dir, script),
   }));
-  const scriptPathData = scriptsPathData.find((data) => {
+  const scriptPathData = scriptsPathData.find(data => {
     const file = fileInfo(data.file);
     if (!file.exists) return false;
-    if (!file.executable) throw tinyError(
-      `Pssst. We can’t run the file \`${relativeScriptPath(data.dir)}\`, because ` +
-      'it’s not an executable. You can grant it execute permissions ' +
-      `by running \`chmod +x ${relativeScriptPath(data.dir)}\`.`
-    );
+    if (!file.executable)
+      throw tinyError(
+        `Pssst. We can’t run the file \`${relativeScriptPath(
+          data.dir
+        )}\`, because ` +
+          'it’s not an executable. You can grant it execute permissions ' +
+          `by running \`chmod +x ${relativeScriptPath(data.dir)}\`.`
+      );
     return true;
   });
 
-  if (scriptPathData === undefined) throw tinyError(
-    'Aw shucks! We’ve searched the whole place, but we can’t find ' +
-    `the script \`${script}\`. Make sure there’s an executable file ` +
-    `at \`${relativeScriptPath(scriptsDirs[0])}\`.`
-  );
+  if (scriptPathData === undefined)
+    throw tinyError(
+      'Aw shucks! We’ve searched the whole place, but we can’t find ' +
+        `the script \`${script}\`. Make sure there’s an executable file ` +
+        `at \`${relativeScriptPath(scriptsDirs[0])}\`.`
+    );
 
-
-  const processData = childProcess.spawnSync(
-    scriptPathData.file,
-    scriptArgs,
-    { stdio: 'inherit' }
-  );
+  const processData = childProcess.spawnSync(scriptPathData.file, scriptArgs, {
+    stdio: 'inherit',
+  });
   return processData.status;
 };
--- test/please.test.js
+++ test/please.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable quote-props */
-  // mock-fs file trees and proxyquire overrides look better when quoted
+// mock-fs file trees and proxyquire overrides look better when quoted
 require('tap-spec-integrated');
 
 const test = require('tape-catch');
@@ -9,40 +9,36 @@ const sinon = require('sinon');
 const path = require('path');
 
 const executable = mockFs.file({
-  mode: parseInt('111', 8),
+  mode: 0o111,
 });
 
-const newProcess = (splat) => Object.assign({
-  cwd: () => '/',
-  stdout: {},
-  stdin: {},
-}, splat);
+const newProcess = splat =>
+  Object.assign(
+    {
+      cwd: () => '/',
+      stdout: {},
+      stdin: {},
+    },
+    splat
+  );
 
-test((
-  'Displays nice `--help`'
-), (is) => {
+test('Displays nice `--help`', is => {
   const status = 5;
-  const spawnSync = sinon.stub().returns({ status });
+  const spawnSync = sinon.stub().returns({status});
   const process = newProcess();
 
   const please = proxyquire('../source/please', {
-    'child_process': { spawnSync },
+    child_process: {spawnSync},
   });
 
-  const exitCode = please(['--help'], { process });
+  const exitCode = please(['--help'], {process});
 
-  is.equal(
-    spawnSync.callCount,
-    1,
-    'spawns…'
-  );
+  is.equal(spawnSync.callCount, 1, 'spawns…');
 
   const command = spawnSync.lastCall.args[0];
   const args = spawnSync.lastCall.args[1];
   const config = spawnSync.lastCall.args[2];
-  is.equal(command, 'man',
-    '…`man`'
-  );
+  is.equal(command, 'man', '…`man`');
 
   is.equal(
     args[0],
@@ -50,78 +46,59 @@ test((
     'with the right manpage'
   );
 
-  is.equal(
-    config.stdio[0],
-    process.stdout,
-    'writes to `stdout`'
-  );
+  is.equal(config.stdio[0], process.stdout, 'writes to `stdout`');
 
-  is.equal(
-    config.stdio[1],
-    process.stdin,
-    'reads from `stdin`'
-  );
+  is.equal(config.stdio[1], process.stdin, 'reads from `stdin`');
 
-  is.equal(exitCode, 0,
-    'succeeds'
-  );
+  is.equal(exitCode, 0, 'succeeds');
   is.end();
 });
 
-test((
-  'Falls back to plain text `--help` over stdout'
-), (is) => {
+test('Falls back to plain text `--help` over stdout', is => {
   const expectedHelpContent = 'I’m dreaming of a white Christmas';
-  const hasbin = { sync: sinon.stub().returns(false) };
-  const fs = { readFileSync: sinon.stub().returns(expectedHelpContent) };
-  const stdout = { write: sinon.spy() };
+  const hasbin = {sync: sinon.stub().returns(false)};
+  const fs = {readFileSync: sinon.stub().returns(expectedHelpContent)};
+  const stdout = {write: sinon.spy()};
 
   const please = proxyquire('../source/please', {
     hasbin,
     fs,
   });
 
-  const exitCode = please(['--help'], { process: newProcess({ stdout }) });
+  const exitCode = please(['--help'], {process: newProcess({stdout})});
 
   is.equal(
-    hasbin.sync.calledOnce &&
-    hasbin.sync.lastCall.args[0],
+    hasbin.sync.calledOnce && hasbin.sync.lastCall.args[0],
     'man',
     'checks if `man` is in the PATH'
   );
 
   const expectedLocation = '../manpages/please.1.txt';
   is.deepEqual(
-    fs.readFileSync.calledOnce &&
-    fs.readFileSync.lastCall.args,
+    fs.readFileSync.calledOnce && fs.readFileSync.lastCall.args,
     [path.resolve(__dirname, expectedLocation), 'utf8'],
     `reads the file at \`${expectedLocation}\``
   );
 
   is.equal(
-    stdout.write.calledOnce &&
-    stdout.write.lastCall.args[0],
+    stdout.write.calledOnce && stdout.write.lastCall.args[0],
     expectedHelpContent,
     'prints the help text'
   );
 
-  is.equal(exitCode, 0,
-    'succeeds'
-  );
+  is.equal(exitCode, 0, 'succeeds');
   is.end();
 });
 
-test((
-  'Runs an executable from a `scripts` directory in the CWD'
-), (is) => {
+test('Runs an executable from a `scripts` directory in the CWD', is => {
   const script = 'my-script';
   const args = ['one two', 'three'];
   const status = 5;
 
-  const spawnSync = sinon.stub().returns({ status });
+  const spawnSync = sinon.stub().returns({status});
 
   const please = proxyquire('../source/please', {
-    'child_process': { spawnSync },
+    child_process: {spawnSync},
   });
 
   mockFs({
@@ -131,7 +108,7 @@ test((
   });
 
   const spawnSyncCallCount = spawnSync.callCount;
-  const exitCode = please([script].concat(args), { process: newProcess() });
+  const exitCode = please([script].concat(args), {process: newProcess()});
 
   is.equal(
     spawnSync.callCount,
@@ -143,30 +120,20 @@ test((
     `/scripts/${script}`,
     'to call the right script'
   );
-  is.deepEqual(
-    spawnSync.lastCall.args[1],
-    args,
-    'with the right arguments'
-  );
+  is.deepEqual(spawnSync.lastCall.args[1], args, 'with the right arguments');
   is.deepEqual(
     spawnSync.lastCall.args[2],
-    { stdio: 'inherit' },
+    {stdio: 'inherit'},
     'printing to stdout/stderr and reading from stdin'
   );
 
-  is.equal(
-    exitCode,
-    status,
-    'exits with the same status as the script'
-  );
+  is.equal(exitCode, status, 'exits with the same status as the script');
 
   mockFs.restore();
   is.end();
 });
 
-test((
-  'Throws an informative error if it can’t find a scripts directory'
-), (is) => {
+test('Throws an informative error if it can’t find a scripts directory', is => {
   is.plan(2);
 
   const process = newProcess();
@@ -177,25 +144,20 @@ test((
   });
 
   try {
-    please(['my-script', 'whatever'], { process });
+    please(['my-script', 'whatever'], {process});
   } catch (error) {
     is.ok(
       /can’t find a scripts directory/i.test(error.message),
       'tells me what’s wrong'
     );
-    is.ok(
-      /make sure/i.test(error.message),
-      'tells me what to do'
-    );
+    is.ok(/make sure/i.test(error.message), 'tells me what to do');
   }
 
   mockFs.restore();
   is.end();
 });
 
-test((
-  'Throws an informative error if it can’t find the script'
-), (is) => {
+test('Throws an informative error if it can’t find the script', is => {
   is.plan(2);
 
   const process = newProcess();
@@ -206,68 +168,61 @@ test((
   });
 
   try {
-    please(['my-script', 'whatever'], { process });
+    please(['my-script', 'whatever'], {process});
   } catch (error) {
     is.ok(
       /can’t find the script `my-script`/i.test(error.message),
       'tells me what’s wrong'
     );
-    is.ok(
-      /make sure/i.test(error.message),
-      'tells me what to do'
-    );
+    is.ok(/make sure/i.test(error.message), 'tells me what to do');
   }
 
   mockFs.restore();
   is.end();
 });
 
-test((
+test(
   'Throws an informative error if the script is not an executable file ' +
-  'or symlink'
-), (is) => {
-  is.plan(2);
+    'or symlink',
+  is => {
+    is.plan(2);
 
-  const process = newProcess();
-  const please = proxyquire('../source/please', {});
+    const process = newProcess();
+    const please = proxyquire('../source/please', {});
 
-  mockFs({
-    '/scripts': {
-      'my-script': '(plain text)',
-    },
-  });
+    mockFs({
+      '/scripts': {
+        'my-script': '(plain text)',
+      },
+    });
 
-  try {
-    please(['my-script', 'whatever'], { process });
-  } catch (error) {
-    is.ok(
-      /can’t run the file `scripts\/my-script`/i.test(error.message),
-      'tells me what’s wrong'
-    );
-    is.ok(
-      /you can/i.test(error.message),
-      'tells me what to do'
-    );
+    try {
+      please(['my-script', 'whatever'], {process});
+    } catch (error) {
+      is.ok(
+        /can’t run the file `scripts\/my-script`/i.test(error.message),
+        'tells me what’s wrong'
+      );
+      is.ok(/you can/i.test(error.message), 'tells me what to do');
+    }
+
+    mockFs.restore();
+    is.end();
   }
+);
 
-  mockFs.restore();
-  is.end();
-});
-
-test((
-  'Looks for executables recursively, crawling up the directory tree'
-), (is) => {
+test('Looks for executables recursively, crawling up the directory tree', is => {
   const script = 'my-script';
   const status = 5;
 
-  const spawnSync = sinon.stub().returns({ status });
+  const spawnSync = sinon.stub().returns({status});
   const cwd = '/current-working-dir';
   const process = newProcess({
     cwd: () => cwd,
   });
 
   const please = proxyquire('../source/please', {
-    'child_process': { spawnSync },
+    child_process: {spawnSync},
   });
 
   mockFs({
@@ -275,13 +230,13 @@ test((
       [script]: executable,
     },
     [cwd]: {
-      'scripts': {
+      scripts: {
         'other-script': executable,
       },
     },
   });
 
-  please([script], { process });
+  please([script], {process});
 
   is.equal(
     !!spawnSync.calledOnce && spawnSync.lastCall.args[0],
@@ -293,14 +248,12 @@ test((
   is.end();
 });
 
-test((
-  'Prints a list of found executables when called without arguments'
-), (is) => {
+test('Prints a list of found executables when called without arguments', is => {
   const scriptOne = 'script-one';
   const scriptTwo = 'script-two';
   const status = 5;
 
-  const spawnSync = sinon.stub().returns({ status });
+  const spawnSync = sinon.stub().returns({status});
   const cwd = '/current-working-dir';
   const stdout = {
     write: sinon.spy(),
@@ -312,38 +265,34 @@ test((
   });
 
   const please = proxyquire('../source/please', {
-    'child_process': { spawnSync },
+    child_process: {spawnSync},
   });
 
   mockFs({
     '/scripts': {
       [scriptTwo]: executable,
-      'directory': {},
+      directory: {},
     },
     [cwd]: {
-      'scripts': {
+      scripts: {
         [scriptOne]: executable,
         'plain-text-file': '(plain text)',
       },
     },
   });
 
-  const callCount = stdout.write.callCount;
-  please([], { process });
+  const {callCount} = stdout.write;
+  please([], {process});
 
-  is.equal(
-    stdout.write.callCount,
-    callCount + 1,
-    'prints to stdout'
-  );
+  is.equal(stdout.write.callCount, callCount + 1, 'prints to stdout');
   is.deepEqual(
     stdout.write.lastCall.args,
     [
       '\n' +
-      'Available commands:\n' +
-      `  ${scriptOne}\n` +
-      `  ${scriptTwo}\n` +
-      '\n',
+        'Available commands:\n' +
+        `  ${scriptOne}\n` +
+        `  ${scriptTwo}\n` +
+        '\n',
     ],
     'prints the right stuff'
   );
